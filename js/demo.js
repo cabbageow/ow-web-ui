d1291645498859faaf39d02523b9634138542de0
wych1987 <wangyunchen@163.com>
2012-07-02 06:57:59 +0800

    修改部分数据，ajax读取硬盘信息

--- js/demo.js
+++ js/demo.js
@@ -1,19 +1,45 @@
 ﻿/*页面交互的js*/
 //ajax读取json数据
 var requestURL = {
-	disk_manager:"json/disk_manager.json",
-	disk_part_check:"json/disk_part_check.json",
-	disk_part_create:"json/disk_part_create.json",
-	disk_part_delete:"json/disk_part_delete.json",
-	disk_part_format:"json/disk_part_format.json"
+	disk_manager : "json/disk_manager.json",
+	disk_part_check : "json/disk_part_check.json",
+	disk_part_create : "json/disk_part_create.json",
+	disk_part_delete : "json/disk_part_delete.json",
+	disk_part_format : "json/disk_part_format.json"
 };
-var resultData={} 
+var resultData = {}
 $.get(requestURL.disk_manager,
-   function(data){
-     eval(data);
-	 alert(10);
-	 console.log(resultData);
-   });
+	function (data) {
+	resultData = eval(data);
+	console.log(resultData);
+	fillHtmlByData(resultData,$("#diskinfo01")[0]);
+});
+
+function fillHtmlByData(data, Dom) {
+	var html = [],Class,percent,typeName;
+	html.push('<div class="box02_1 red j_diskPart" style="width:' + data[0].parts[0].range_percent + '">' + data[0].parts[0]._comment_1 + '</div>');
+	html.push('<div class="box02_1 green w500" style="width:' + data[0].parts[1].range_percent + '">');
+	for (var i = 0; i < data[0].parts[1].parts.length; i++) {
+		switch(data[0].parts[1].parts[i].type){
+			case "logical"://逻辑分区
+				Class ="yellow j_diskPart";
+			break;
+			case "empty":
+			Class ="block j_diskSpace";
+			break;
+			default:
+			break;
+		}
+		percent = data[0].parts[1].parts[i].range_percent;
+		typeName = data[0].parts[1].parts[i].type_name;
+		html.push('<div class="box02_1 w150 h30 box02_2 '+Class+'"style="width:'+percent+'">');
+		html.push(typeName+'</div>');	
+	}
+	html.push("</div>");
+	Dom.innerHTML = html.join('');
+	console.log(html.join(''));
+	console.log(Dom.innerHTML);
+}
 $("#dialog-message" ).dialog({
 			autoOpen:false,
 			modal: true,
