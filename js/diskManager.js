9185860d7072636aa50bc3e7a79cdc24cf096904
wych1987 <wangyunchen@163.com>
2012-07-03 00:46:21 +0800

    数据的读取显示做完了，点击显示也做了，明天做按钮的点击事件。

--- js/diskManager.js
+++ js/diskManager.js
@@ -12,67 +12,82 @@ $.get(requestURL.disk_manager,
 	function (data) {
 	resultData = eval(data);
 	//console.log(resultData);
-	wycFun.fillColorHtmlByData(resultData,$("#diskinfo01")[0]);
+	wycFun.fillColorHtmlByData(resultData[0],$("#diskinfo01")[0],0);
 });
 var wycFun = {
 	getPercent : function (sum, num) { //获取百分比的函数
 			return (Math.round((num / sum) * 100));
 	},
-	fillColorHtmlByData : function (data, Dom) {//填充图像颜色
-		var Class,percent,typeName,html = [];
-		percent = this.getPercent(data[0].range['end'],data[0].parts[0].range['end']);
-		percent = percent -1;//少1像素的区域留给边框
-		html.push('<div class="box02_1 red j_diskPart" style="width:' + percent + '%"dataindex =resultData[0].parts[0] boolClick="true">' + data[0].parts[0].type_name+data[0].parts[0].num + '</div>');
-		//percent = percent-1;
-		var othersDisk = data[0].range['end'] - data[0].parts[0].range['end'];
-		html.push('<div class="box02_1 green w500" style="width:' + percent + '%">');
-		for (var i = 0; i < data[0].parts[1].parts.length; i++) {
-			switch (data[0].parts[1].parts[i].type) {
-			case "logical": //逻辑分区
-				Class = "yellow j_diskPart";
-				break;
-			case "empty":
-				Class = "block j_diskSpace";
-				break;
-			default:
-				break;
+	/*getExtenedPercent:function(data){//获取本次盘有多少逻辑分区
+		var num = 0;
+		for(var k = 0; k <data.parts.length; k++){
+			if(data.parts[k].type=='extened'){
+				num +=data.parts[k].range['end']-data.parts[k].range['start'];
 			}
-			console.log(data[0].parts[1].parts[i].range['end']);
-			percent = this.getPercent(othersDisk,data[0].parts[1].parts[i].range['end']- data[0].parts[1].parts[i].range['start']+1);
-			percent = percent-1;//少1像素的区域留给边框
-			typeName = data[0].parts[1].parts[i].type_name;
-			html.push('<div class="box02_1 w150 h30 box02_2 ' + Class + '"style="width:' + percent + '%"dataindex=resultData[0].parts[1].parts['+i+'] boolClick="true">');
-			html.push(typeName + data[0].parts[1].parts[i].num+'</div>');
 		}
-		html.push("</div>");
-		Dom.innerHTML = html.join('');
-	}
-}
-/*function fillHtmlByData(data, Dom) {
-	var html = [],Class,percent,typeName;
-	html.push('<div class="box02_1 red j_diskPart" style="width:' + data[0].parts[0].range_percent + '">' + data[0].parts[0]._comment_1 + '</div>');
-	html.push('<div class="box02_1 green w500" style="width:' + data[0].parts[1].range_percent + '">');
-	for (var i = 0; i < data[0].parts[1].parts.length; i++) {
-		switch(data[0].parts[1].parts[i].type){
-			case "logical"://逻辑分区
-				Class ="yellow j_diskPart";
-			break;
-			case "empty":
-			Class ="block j_diskSpace";
-			break;
-			default:
-			break;
-		}
-		percent = data[0].parts[1].parts[i].range_percent;
-		typeName = data[0].parts[1].parts[i].type_name;
-		html.push('<div class="box02_1 w150 h30 box02_2 '+Class+'"style="width:'+percent+'">');
-		html.push(typeName+'</div>');	
-	}
-	html.push("</div>");
-	Dom.innerHTML = html.join('');
-	console.log(html.join(''));
-	console.log(Dom.innerHTML);
-}*/
+		return num;
+	}*/
+	fillColorHtmlByData : function (data, Dom, Num) { //填充图像颜色,硬盘数据对象，对应的dom元素，硬盘数据对象的索引号
+		var Class,
+		percent,
+		typeName,
+		html_primary = [],
+		html_extened = [];
+		var diskSumNum = data.range['end'];
+		var diskExtenedSum = data.extened_range_sum;
+		percent = this.getPercent(diskSumNum, diskExtenedSum) -1;
+		html_extened.push('<div class="box02_1 green w500" style="width:' + percent + '%">');
+			for (var k = 0; k < data.parts.length; k++) {
+				switch (data.parts[k].type) {
+				case 'extened': //逻辑分区
+					for (var i = 0; i < data.parts[k].parts.length; i++) {
+						switch (data.parts[k].parts[i].type) {
+						case "logical": //逻辑分区
+							Class = "yellow j_diskPart";
+							break;
+						case "empty":
+							Class = "block j_diskSpace";
+							break;
+						default:
+							break;
+						}
+						console.log(data.parts[k].parts[i].range['end']);
+						percent = this.getPercent(diskExtenedSum, data.parts[k].parts[i].range['end'] - data.parts[k].parts[i].range['start'] + 1);
+						percent = percent - 1; //少1像素的区域留给边框
+						typeName = data.parts[k].parts[i].type_name;
+						html_extened.push('<div class="box02_1 w150 h30 box02_2 ' + Class + '"style="width:' + percent + '%"dataindex=resultData['+Num+'].parts[' + k + '].parts[' + i + '] boolClick="true">');
+						html_extened.push(typeName + data.parts[k].parts[i].num + '</div>');
+					}
+					break;
+				case 'primary': // 主分区
+					percent = this.getPercent(diskSumNum, data.parts[k].range['end'] - data.parts[k].range['start'] + 1);
+					percent = percent - 1;
+					html_primary.push('<div class="box02_1 red j_diskPart" style="width:' + percent + '%"dataindex =resultData['+Num+'].parts[' + k + '] boolClick="true">' + data.parts[k].type_name + data.parts[k].num + '</div>');
+					break;
+				default:
+					break;
+				}
+			}
+				html_extened.push("</div>");
+				Dom.innerHTML = html_primary.join('') + html_extened.join('');
+			},
+	fillDataTotable:function(data,dom){
+		var html_table = ['<table class="table1"><tbody><tr><td class="td1">分区类型:</td><td>'];
+		html_table.push(data.type_name);
+		html_table.push('</td><td class="td1">设备:</td><td>');
+		html_table.push(data.dev);
+		html_table.push('</td></tr><tr><td class="td1">文件系统:</td><td>');
+		html_table.push(data.fstype);
+		html_table.push('</td><td class="td1">挂载点:</td><td>');
+		html_table.push(data.mountpoint);
+		html_table.push('</td></tr><tr><td class="td1">容量:</td><td>');
+		html_table.push(data.size.total);
+		html_table.push('</td><td class="td1">剩余空间:</td><td>');
+		html_table.push(data.size.left)
+		html_table.push('</td></tr></tbody></table>');
+		dom.innerHTML = html_table.join('');
+	}	
+}//读取函数完毕		
 $("#dialog-message" ).dialog({
 			autoOpen:false,
 			modal: true,
@@ -128,3 +143,44 @@ $(".j_diskPart").toggle(function(){
 	
 },function(){$("#diskFormat").hide();}
 );
+//下面写点击事件的处理
+$('#diskinfo01').bind('click',function(event){
+	
+	e = event||window.event;
+	var target = event.target ||event.srcElement;
+	var dataindex = target.getAttribute('dataindex');
+	if(dataindex){
+	console.log(dataindex);
+	 dataindex = eval(dataindex);
+	//console.log(dataindex.dev);
+	wycFun.fillDataTotable(dataindex,$("#diskInfoTable")[0]);
+	$("#diskFormat").show();
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
