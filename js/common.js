fa5ecd344939052128579bab99cc6753b9dd3a73
wych1987 <wangyunchen@163.com>
2012-07-06 00:18:24 +0800

    使用框架来控制页面的跳转

---
+++ js/common.js
@@ -0,0 +1,14 @@
+﻿var commonFunc = {
+	changeTheHrefByHash:function(){
+		//var href = top.location.hash.substring(1);
+		if(href !==top.name){
+		var href = top.name;
+		console.log(href);
+		var regHref = /\.html$/;//一个简单的匹配
+		if(regHref.test(href)){
+			window.location.href = href;
+		}
+	}
+	}
+};
+setInterval(commonFunc.changeTheHrefByHash,1000);
